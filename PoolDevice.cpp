eb9c1307682c3e7edce0e4d4823028708657875e
Frank Clements <fclements@MacBook-Pro-2.local>
2019-01-10 08:59:45 -0500

    Created the PoolDevice class and code to use it in pool-controller
    The PoolDevice class binds the pin on the wemos to the Sinric deviceId
    and wraps up all the logic for firing the associated pins.

---
+++ PoolDevice.cpp
@@ -0,0 +1,78 @@
+
+/*
+DebugUtils.h - Simple debugging utilities.
+Copyright (C) 2011 Fabio Varesano <fabio at varesano dot net>
+
+Ideas taken from:
+http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1271517197
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "PoolDevice.h"
+
+PoolDevice::PoolDevice(int panelPin, String deviceId) {
+  this->_panelPin = panelPin;
+  this->_deviceId = deviceId;
+  this->_currentStatus = PoolDeviceStatus::OFF;
+
+  pinMode(this->_panelPin, OUTPUT);
+}
+
+// Flip the status of the _currentStatus
+PoolDeviceStatus PoolDevice::_changeStatus() {
+  if(this->_currentStatus == OFF) {
+    this->_currentStatus = ON;
+  }
+  else {
+    this->_currentStatus = OFF;
+  }
+  
+  return this->_currentStatus;
+}
+
+// Simulate a button press for 0.5 seconds and then set the current stat opposite to current state.
+PoolDeviceStatus PoolDevice::_firePin() {
+  digitalWrite(this->_panelPin, HIGH);
+  delay(500);
+  digitalWrite(this->_panelPin, LOW);
+
+  this->_changeStatus();
+  return this->_currentStatus;
+}
+
+// Return the object's device ID
+String PoolDevice::getDeviceId() {
+  return this->_deviceId;
+}
+
+// Turn on the device
+void PoolDevice::turnOn() {
+  if(this->_currentStatus == OFF) {
+    this->_firePin();
+    this->_changeStatus();
+  }
+
+  return;
+}
+
+// Turn off the device
+void PoolDevice::turnOff() {
+  if(this->_currentStatus == ON) {
+    this->_firePin();
+    this->_changeStatus();
+  }
+
+  return;
+}
---
+++ PoolDevice.h
@@ -0,0 +1,51 @@
+/**
+ * PoolDevice.h - Library for controlling pool andn spa functions.
+ *
+ * Created by Frank W Clements, January 3, 2019.
+ * Released into the public domain.
+ *
+ */
+#ifndef PoolDevice_h
+#define PoolDevice_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+  #include "pins_arduino.h"
+  #include "WConstants.h"
+#endif
+
+enum PoolDeviceMode
+{
+  UNKNOWN,
+  ON,
+  OFF
+};
+
+class PoolMode
+{
+  public:
+    PoolMode(int panelStatusPin);
+    PoolDeviceMode getCurrentMode();
+  private:
+    PoolDeviceMode _currentMode;
+};
+
+class PoolDevice
+{
+  public:
+    PoolDevice(int panelPin, String deviceId);
+    void turnOn();
+    void turnOff();
+    String getDeviceId();
+
+  private:
+    int _panelPin;
+    String _deviceId;
+    PoolDeviceStatus _currentStatus;
+    PoolDeviceStatus _firePin();
+    PoolDeviceStatus _changeStatus();
+};
+
+#endif
