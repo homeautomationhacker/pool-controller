450248dc71c24d9672a12df8a6a06303cd40fb54
Frank Clements <frank@sixthtoe.net>
2019-01-10 09:04:08 -0500

    Commiting pool-controller changes

--- pool-controller.ino
+++ pool-controller.ino
@@ -33,12 +33,14 @@
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 #include <StreamString.h>
+#include "PoolDevice.h"
 
 // Enable debugging?  if not, comment this out and save cycles at runtime
 #define DEBUG
 #ifdef DEBUG
   #define DEBUG_PRINTLN(x) Serial.println(x);
   #define DEBUG_PRINT(x) Serial.print(x);
+  #define DEBUG_PRINTF(x, y) Serial.printf(x, y);
 #else
   #define DEBUG_PRINTLN(x)
   #define DEBUG_PRINT(x)
@@ -57,6 +59,9 @@
 #define MY_API_KEY      "xxxxxxxx-xxx-xxxx-xxxx-xxxxxxxxxxxx" // found on the https://sinric.com site
 #define SINRIC_DEVICE1  "<replace with your device id here>"  // each device has a unique ID - rinse repeat for each
 #define SINRIC_DEVICE2  "<replace with your device id here>"
+#define SINRIC_DEVICE3  "<replace with your device id here>"
+#define SINRIC_DEVICE4  "<replace with your device id here>"
+#define MAX_POOL_DEVICES  4   // Maximum number of devices to support
 
 // Wifi parameters
 #define MY_SSID           "Your SSID Goes Here"     // Your SSID to connect to
@@ -71,31 +76,33 @@ ESP8266WiFiMulti wifiMulti;
 WebSocketsClient webSocket;
 WiFiClient client;
 
-bool isConnected = false;           // Is the Wemos connected to the WiFi network?
-
+bool isConnected = false;                           // Is the Wemos connected to the WiFi network?
+const int JSON_BUFFER_SIZE = JSON_OBJECT_SIZE(3);   // The size of our JSON responses from Sinric is 3 objects long
 
 // Declare functions used in setup() and loop() - they're implemented at the end so they need to be declared here
-//void turnOn(char *deviceId);
-//void turnOff(char *deviceId);
+void firePin(int triggerPin);
+void turnOn(String deviceId);
+void turnOff(String deviceId);
 void webSocketEventCallback(WStype_t type, uint8_t *payload, size_t length);
 
 
+// PoolDevice class binds the physical AUX port on the pool panel to the Sinric device
+// Instantiate a PoolDevice for each auxillary pin/sinric device
+PoolDevice aux1 = PoolDevice(AUX_1, SINRIC_DEVICE1);
+PoolDevice aux2 = PoolDevice(AUX_2, SINRIC_DEVICE2);
+PoolDevice aux3 = PoolDevice(AUX_3, SINRIC_DEVICE3);
+PoolDevice aux4 = PoolDevice(AUX_4, SINRIC_DEVICE4);
+PoolDevice *poolDevices[MAX_POOL_DEVICES] = {&aux1, &aux2, &aux3, &aux4};
 
 // Main micro controller seutp 
 void setup() {
   Serial.begin(115200);
 
-  // All the auxiliary pins are output pins that are used to fire an optocoupler
-  pinMode(AUX_1, OUTPUT);
-  pinMode(AUX_2, OUTPUT);
-  pinMode(AUX_3, OUTPUT);
-  pinMode(AUX_4, OUTPUT);
-
   // The status LED from the panel would be the only input pin used to determine the current status of the panel
   pinMode(STATUS_LED, INPUT);
 
-
-  // Setup the WiFI connection
+  // 
+  // Setup the WiFI connection and initiate the connection
   wifiMulti.addAP(MY_SSID, MY_WIFI_PASSWORD);
   
   // Wait while the board connects to the network - foreverever, foreverever, if it never can 
@@ -118,6 +125,8 @@ void setup() {
   webSocket.onEvent(webSocketEventCallback);  // Set the event handler callback and API key
   webSocket.setAuthorization("apiKey", MY_API_KEY); // Set the API key for use
   webSocket.setReconnectInterval(5000);       // Set the reconnect interval to 5 seconds
+  // end wifi setup 
+
 }
 
 // Micro controller loop function
@@ -153,20 +162,82 @@ void loop() {
 void webSocketEventCallback(WStype_t type, uint8_t *payload, size_t length) {
   switch(type)
   {
-    case WStype_DISCONNECTED:
+    case WStype_DISCONNECTED: {
       isConnected = false;
-      DEBUG_PRINTLN("[WebSocketCallback] Webservice disconnected from " + SINRIC_HOST);
+      DEBUG_PRINT("[WebSocketCallback] Webservice disconnected from ");
+      DEBUG_PRINTLN(SINRIC_HOST);
       break;
-    case WStype_CONNECTED:
+    }
+    case WStype_CONNECTED: {
       isConnected = true;
-      DEBUG_PRINTLN("[WebSocketCallback] Webservice connected to " + SINRIC_HOST);
-      DEBUG_PRINLN("Waiting for commands from " + SINRIC_HOST);
+      DEBUG_PRINT("[WebSocketCallback] Webservice connected to ");
+      DEBUG_PRINTLN(SINRIC_HOST);
+      DEBUG_PRINTLN("Waiting for commands from ");
+      DEBUG_PRINTLN(SINRIC_HOST);
       break;
-    case WStype_TEXT:
+    }
+    case WStype_TEXT: {
       // Parse the response from the Sinric API to figure out what device to call
+      DEBUG_PRINTF("[WebSocketCallback] Got text response of: %s\n", payload);
+      DEBUG_PRINTF("[WebSocketCallback] Lenght of message %d bytes\n", length);
+      
+      StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+      JsonObject &json = jsonBuffer.parseObject((char*)payload);
+
+      String deviceId = json["deviceId"];
+      String action = json["action"];
+      PoolDevice *poolDevice = NULL;
+
+      // find the matching pool device object
+      for(int i = 0; i < MAX_POOL_DEVICES; i++) {
+        if(poolDevices[i]->getDeviceId() == deviceId) {
+          poolDevice = poolDevices[i];
+        }
+      }
+
+      if(poolDevice != NULL) {
+        // This only reacts to setPowerState events, and tests for logging purpose.
+        if(action == "setPowerState") 
+        {
+          String value = json["value"];
+          if(value == "ON")
+          {
+            poolDevice->turnOn();
+          } else {
+            poolDevice->turnOff();
+          }
+        }
+        else if(action == "test")
+        {
+          DEBUG_PRINTLN("[WebSocketCallback] received test command from Sinric");
+        }
+      }
       break;
+    }
+    case WStype_ERROR: {
+      DEBUG_PRINTLN("[WebSocketCallback] there was some kind of an error or unknown response in the payload type");
+      break;
+    }
+    // Various other unhandled response types
+    case WStype_FRAGMENT_BIN_START: {
+      DEBUG_PRINT("[WebSocketCallback] received binary data start");
+      break;
+    }
+    case WStype_FRAGMENT_TEXT_START: {
+      DEBUG_PRINT("[WebSocketCallback] received fragmented text start");
+      break;
+    }
+    case WStype_BIN: {
+      DEBUG_PRINT("[WebSocketCallback] received binary data");
+      break;
+    }
+    case WStype_FRAGMENT: {
+      DEBUG_PRINT("[WebSocketCallback] received fragment");
+      break;
+    }
+    case WStype_FRAGMENT_FIN: {
+      DEBUG_PRINT("[WebSocketCallback] received fragment text stop");
+      break;
+    }
   }
-  // TODO: Used later
-  // const int JSON_BUFFER_SIZE = JSON_OBJECT_SIZE(3);
-  // StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
 }
